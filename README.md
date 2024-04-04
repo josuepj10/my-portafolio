ed4673dcbeca62869cc09b261902dc448fe7b5d4
josuepj1001 <josue.perez@uned.cr>
2024-04-03 20:16:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+
+## Install dependencies
+
+```
+1. npx create-next-app@latest
+
+2. npm install @heroicons/react
+
+3. npm install --save-dev framer-motion three react-intersection-observer @react-three/drei #react-three/fiber
+
+4. npm install react-icons
+```
+
---
+++ constants/index.ts
@@ -0,0 +1,262 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/images/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/images/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/images/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/images/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/images/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/reactquery.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/images/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/images/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/images/framer.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: "/images/stripe.webp",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: "/images/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/images/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/images/discord.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/images/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/images/instagram.svg",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/images/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/images/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/images/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/images/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Material UI",
+      Image: "/images/mui.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/images/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/images/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js",
+      Image: "/images/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Bootstrap",
+      Image: "/images/bootstrap.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Vue js",
+      Image: "/images/vue.png",
+      width: 70,
+      height: 70,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: "/images/node-js.png",
+      width: 80,
+      height: 80,
+    },   
+    {
+      skill_name: "Fire base",
+      Image: "/images/Firebase.png",
+      width: 55,
+      height: 55,
+    },    
+    {
+      skill_name: "PHP",
+      Image: "/images/php.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "C#",
+      Image: "/images/c.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Slim PHP",
+      Image: "/images/slim.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Twig PHP",
+      Image: "/images/twig.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Laravel",
+      Image: "/images/laravel.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Nest Js",
+      Image: "/images/nest.png",
+      width: 70,
+      height: 70,
+    },
+  
+  ];
+  
+  export const Data_base = [
+    {
+      skill_name: "Mongo db",
+      Image: "/images/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/images/mysql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Heidi Sql",
+      Image: "/images/heidisql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Mariadb",
+      Image: "/images/mariadb.png",
+      width: 70,
+      height: 70,
+    },
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Linux",
+      Image: "/images/linux.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Photoshop",
+      Image: "/images/photoshop.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Premiere",
+      Image: "/images/premiere.png",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  
+  
+  
+  
